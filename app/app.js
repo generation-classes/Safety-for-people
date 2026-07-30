f53b8027c8ac58f20cacb4f3792f29cc8f9fff22
Lizeth_caro <lizethandreacarosilva@gmail.com>
2026-07-30 14:11:31 -0500

    chore: remove outdated about us page and associated images

---
+++ app/app.js
@@ -0,0 +1,11 @@
+const App = {
+    pages: {
+        inicio: 'index.html',
+        productos: 'pages/productos.html',
+        categorias: 'pages/categorias.html',
+        aboutUs: 'pages/about-us.html',
+        contacto: 'pages/contacto.html',
+    },
+};
+
+window.App = App;
